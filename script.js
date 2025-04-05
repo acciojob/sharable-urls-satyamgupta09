2d1fce592619783edfb631fdda09949df780ec01
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-05 20:57:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// your code here
+
+document.getElementById("button").addEventListener('click',function(){
+	let name = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+// https://localhost:8080/
+	let url = document.getElementById("url");
+	if(name){
+		url.innerHTML+=`?name=${name}`;
+	}
+	if(year){
+		url.innerHTML+=`?year=${year}`;
+	}
+	if(year && name){
+		url.innerHTML+=`?name=${name}&year=${year}`;
+	}
+})
